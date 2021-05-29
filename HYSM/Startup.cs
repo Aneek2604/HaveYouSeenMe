318d190940bfea67ca49f82b141640364ea54859
ANEEK MAHATA <aneekmahata@gmail.com>
2021-05-29 20:17:32 +0530

    Add project files.

---
+++ HYSM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HYSM.Startup))]
+namespace HYSM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
